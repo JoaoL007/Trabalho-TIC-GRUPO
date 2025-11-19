59f0109d0e1e211b93729bb3e26ea695e56ab0d5
Eddiet40 <edu31ude@gmail.com>
2025-11-19 11:47:51 +0000

    alteração da navegação

--- pages/noticias.php
+++ pages/noticias.php
@@ -14,21 +14,6 @@
 <?php include '../includes/header.php'; ?>
 <?php include '../includes/nav.php'; ?>
 </php>
-<nav class="main-nav">
-    <div class="container">
-        <div class="nav-wrapper">
-            <a href="index.php" class="nav-logo">
-                <img src="assets/images/logo-simple.svg" alt="RoboNews Logo" class="logo-img">
-                <span class="logo-text">RoboNews</span>
-            </a>
-            <ul class="nav-menu">
-                <li><a href="index.php">Início</a></li>
-                <li><a href="pages/noticias.php">Notícias</a></li>
-                <li><a href="pages/sobre.php">Sobre</a></li>
-            </ul>
-        </div>
-    </div>
-</nav>
 <body>
     <main class="news-main">
         <!-- Hero Section com Slider -->
--- pages/sobre.php
+++ pages/sobre.php
@@ -13,19 +13,4 @@ include '../includes/nav.php';
         </section>
     </div>
 </main>
-<nav class="main-nav">
-    <div class="container">
-        <div class="nav-wrapper">
-            <a href="index.php" class="nav-logo">
-                <img src="assets/images/logo-simple.svg" alt="RoboNews Logo" class="logo-img">
-                <span class="logo-text">RoboNews</span>
-            </a>
-            <ul class="nav-menu">
-                <li><a href="index.php">Início</a></li>
-                <li><a href="pages/noticias.php">Notícias</a></li>
-                <li><a href="pages/sobre.php">Sobre</a></li>
-            </ul>
-        </div>
-    </div>
-</nav>
 <?php include '../includes/footer.php'; ?>
